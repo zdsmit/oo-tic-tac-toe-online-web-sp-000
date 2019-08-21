d185136995b2e85994a6eb0d3c12c22a3bfd243a
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:35:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,91 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
 
+  def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
+  def input_to_index(user_input)
+    (user_input.to_i) - 1
+  end
+
+  def move(board, index, value)
+    board[index] = value
+  end
+
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+  end
+
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+
+  def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    index
+    if valid_move?(board, index) && !position_taken?(board, index)
+      move(board, index, (current_player(board)))
+    else
+      turn(board)
+    end
+    display_board(board)
+  end
+
+  def turn_count(board)
+    board.count { |token| token == 'X' || token == 'O' }
+  end
+
+  def current_player(board)
+    turn_count(board).even? ? "X" : "O"
+  end
+
+
+  def won?(board)
+    WIN_COMBINATIONS.detect do |combo|
+      board[combo[0]] == board[combo[1]] &&
+        board[combo[1]] == board[combo[2]] &&
+        position_taken?(board, combo[0])
+    end
+  end
+
+  def full?(board)
+    board.all? do|position|
+      if position == "X" || position == "O"
+        true
+      end
+    end
+  end
+
+  def draw?(board)
+    if full?(board) && !won?(board)
+      true
+    end
+  end
+
+  def over?(board)
+    if draw?(board) || full?(board) || won?(board)
+      true
+    end
+  end
+
+  def winner(board)
+    WIN_COMBINATIONS.detect do |combination|
+      if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+        return "X"
+      elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+        return "O"
+      elsif draw?(board)
+        return "nil"
+      end
+    end
+  end
+
 end
