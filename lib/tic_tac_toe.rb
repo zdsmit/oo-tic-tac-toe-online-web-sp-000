75c6e842f09d168229345c9fd2031dccf60a590f
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:36:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ class TicTacToe
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  end
 
   def play(board)
   turn(board) until over?(board)
