b85539326bd7c6ad0ee930d47e24f0521af98cf7
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:45:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ class TicTacToe
   end
 
   def turn_count
-    @board.count {|token| token == 'X' || token == 'O' }
+    @board.count {|token| token == 'X' || token == 'O'}
   end
 
   def current_player
