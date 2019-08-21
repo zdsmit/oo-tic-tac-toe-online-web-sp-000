13db32de4dc2a2497124b517623f05862bbefccc
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:53:30 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,99 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
 
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    index
+    if valid_move?(index) && !position_taken?(index)
+      move(index, (current_player))
+    else
+      turn
+    end
+    display_board
+  end
+
+  def play
+    turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+  def input_to_index(user_input)
+    (user_input.to_i) - 1
+  end
+
+  def move(index, value)
+    @board[index] = value
+  end
+
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    @board.count {|token| token == 'X' || token == 'O'}
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+        @board[combo[1]] == @board[combo[2]] &&
+        position_taken?(combo[0])
+    end
+  end
+
+  def full?
+    @board.all? do|position|
+      if position == "X" || position == "O"
+        true
+      end
+    end
+  end
+
+  def draw?
+    if full? && !won?
+      true
+    end
+  end
+
+  def over?
+    if draw? || full? || won?
+      true
+    end
+  end
+
+  def winner
+    WIN_COMBINATIONS.detect do |combination|
+      if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X"
+        return "X"
+      elsif @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
+        return "O"
+      elsif draw?
+        return "nil"
+      end
+    end
+  end
+
 end
