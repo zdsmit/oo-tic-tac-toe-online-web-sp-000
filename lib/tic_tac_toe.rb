3c9df1fb985a1327e27e3cd7fd6c47747d4d5b20
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:43:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
