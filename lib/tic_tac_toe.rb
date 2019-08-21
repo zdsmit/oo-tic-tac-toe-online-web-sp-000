05317631645217525635185bce5a763a5b45903c
Zach  Smith <zdsmith001@gmail.com>
2019-08-21 18:36:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,14 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
 
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
   def play(board)
   turn(board) until over?(board)
   if won?(board)
